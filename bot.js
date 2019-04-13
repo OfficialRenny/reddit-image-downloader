49259569bf8729088664605249e386fcd61a3c38
OfficialRenny <renaldas.sorys@gmail.com>
2019-04-13 02:11:27 +0100

    Slight optimisations

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
-var subreddit = 'pics';
-var timeperiod = 'all'; //
+var subreddit = 'pics'; //Subreddit name without the 'r/'
+var timeperiod = 'all'; //Must be one of these: hour, day, week, month, year, all
 
 const fs = require('fs');
 const http = require('http');
 require('dotenv').config();
 const Snoowrap = require('snoowrap');
 var wget = require('node-wget');
-
+var re = /(?:\.([^.]+))?$/;
+var validFiletypes = ["png", "jpg", "jpeg", "bmp"];
 const r = new Snoowrap({
     userAgent: 'Post downloader bot by /u/xlet_cobra',
     clientId: process.env.CLIENT_ID,
@@ -18,12 +19,14 @@ const r = new Snoowrap({
 r.getSubreddit(subreddit).getTop({time: timeperiod}).fetchAll().then((res) => {
 	console.log(`There is a total of ${res.length} submissions.`);
 	function download(index) {
+		if (index > res.length) return;
 		curPost = res[index];
 		if (curPost == null || undefined) return download(index + 1);
-		if ((curPost['is_self'] == false) && !(curPost['is_self'] == null || undefined)) {
+		if ((curPost['is_self'] == false) && !(curPost['is_self'] == null || undefined) && validFiletypes.contains(re.exec(curPost.url).toLowerCase())) {
+			var ext = re.exec(curPost.url);
 			wget({
 					url: curPost['url'],
-					dest: './images/',
+					dest: `./images/${curPost.title}.${ext}`,
 					timeout: 60000
 				}, function (error, response, body) {
 					if (error) {
@@ -34,7 +37,9 @@ r.getSubreddit(subreddit).getTop({time: timeperiod}).fetchAll().then((res) => {
 						download(index + 1);
 					}
 					});
+		} else {
+			download(index + 1);
 		}
 	}
-	//download(0);
+	download(0);
 });
